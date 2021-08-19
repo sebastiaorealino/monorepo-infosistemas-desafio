5872d4e90115f91ada5420e863b6bf4a5870fc1d
sebastiao edelcio oliveira realino <sebastiaorealino@outlook.com>
2021-08-19 03:39:36 -0300

    feat(env): fix dev environment

--- src/config/environment/development.js
+++ src/config/environment/development.js
@@ -8,10 +8,10 @@ export default {
   // MongoDB connection options
   mongo: {
     // Mongo Server
-    uri: 'mongodb://localhost:27017/chippix', 
+    uri: 'mongodb://localhost:27017/infocars', 
   },
   // secretKey for JWT
-  secretKey: 'BR00Th3RB00K_5Zu5Ak',
+  secretKey: 'INF051573M45_5Zu5Ak_DEV',
   paginateOptions: {
     limit: 15,
   },
